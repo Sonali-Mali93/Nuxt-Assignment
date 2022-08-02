2228f0683471bbef14eaf864b71b8b19de541b4c
Sonali Mali <sonali.mali@olacabs.com>
2022-08-02 15:45:23 +0530

    Added card for displaying the slot time

--- components/slots.vue
+++ components/slots.vue
@@ -2,35 +2,45 @@
     <div>
         <div @click="addSlots()" class="flex mb-4 space-x-2 text-3xl font-bold bg-pink-400 ">click Here to see the
             available time slots</div>
-        <div v-for="(per) in timeSlots" :key="per" class="flex flex-row border border-black w-1/2 mt-2 ">
+        <!-- <div v-for="(per) in timeSlots" :key="per" class="flex flex-row border border-black w-1/2 mt-2 ">
             <p class="basis-1/4">Start Time = {{ per.startTime }} </p>
-            <p class="basis-1/4">End Time  = {{ per.endTime }}</p>
-            <!-- <button class="border border-gray-400 px-3 py-1 max-w-xs rounded bg-red-600 text-white" type="submit"
-                    value="Submit"><NuxtLink to="./demo.vue">Book Now</NuxtLink> </button> -->
+            <p class="basis-1/4">End Time ={{ per.endTime }}</p>
+            
             <NuxtLink to="demo" class="border border-gray-400 px-3 py-1 max-w-xs rounded bg-red-600 text-white">Book Now
             </NuxtLink>
+            <button @click="myFunction()">Book </button>
+        </div> -->
+        <!-- Cards start -->
+        <div v-for="(per) in timeSlots" :key="per" class="flex flex-row space-y-3"> <!---main card div-->
+            <div class=" flex flex-col  max-w-sm rounded overflow-hidden shadow-lg">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Start Time -  {{ per.startTime }}</div>
+                    <div class="font-bold text-xl mb-2">Last Time - {{ per.endTime }}</div>
+                    <div class="font-bold text-xl mb-2"></div>
+
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <button @click="myFunction()" class="border border-gray-400 px-3 py-1 max-w-xs rounded bg-red-600 text-white ">Book Now </button>
+                </div>
+            </div>
         </div>
+
     </div>
+    <!--End of Div-->
+
 </template>
 <script>
 export default {
     data() {
         return {
-            timeSlots: [ ],
+            timeSlots: [],
             i: 0
         }
     },
     methods: {
-        // addSlots(){
-        //     for(i=0 ; i<=20 ; i++){
-        //         var s = new Date();
-        //         console.log(s)
-        //         s.setMinutes(s.getMinutes()+5);
-        //     }
-        // }
         addSlots() {
             var d1 = new Date('2022-08-02 08:50:00');
-            // var d2 = new Date('2022-08-02 09:00:00');
+            var d2 = new Date('2022-08-02 09:00:00');
             for (let i = 0; i < 20; i++) {
                 d1.setMinutes(d1.getMinutes() + 10);
                 d2.setMinutes(d2.getMinutes() + 10);
@@ -38,66 +48,34 @@ export default {
                     startTime: d1.toLocaleTimeString(),
                     endTime: d2.toLocaleTimeString()
                 })
-                // console.log("Current Date and Time =",d1)
-                // console.log("Time",d1.toLocaleTimeString())
-                // this.timeSlots.push(d1.toLocaleTimeString())
-                // this.timeSlots.push(this.initialTime)
-                // this.initialTime.startTime = this.timeSlots[index].startTime;
-                // this.timeSlots.push(this.initialTime)
             }
             console.log(timeSlots)
-
-            },
-
-            //  addSlots() {
-            //     var d;
-            //     d = new Date('2022-08-02 09:00:00');
-            //     // this.timeSlots.push(this.initialTime);
-            //     console.log("timeSlots", this.timeSlots)
-            //     for (let i = 0; i < 20; i++) {
-            //         d.setMinutes(d.getMinutes() + 10);
-            //         // d.getTime()
-            //         this.timeSlots[this.startTime] = (d.toLocaleTimeString())
-            //         console.log("Start Time",this.startTime )
-            //         // this.timeSlots.push(d)
-            //         console.log("time", d.toLocaleTimeString())
-            //         this.timeSlots.push(this.initialTime);
-
-            //     }
-            //     console.log("newtime", this.timeSlots)
-
-            // }
+        },
+        myFunction() {
+            let text;
+            let Name = prompt("Please enter your name:", "Harry Potter");
+            let Mobile = prompt("Please enter your mobile number:", "Harry Potter");
+            document.getElementById("demo").innerHTML = text;
         }
-    }
-</script>
+        //  addSlots() {
+        //     var d;
+        //     d = new Date('2022-08-02 09:00:00');
+        //     // this.timeSlots.push(this.initialTime);
+        //     console.log("timeSlots", this.timeSlots)
+        //     for (let i = 0; i < 20; i++) {
+        //         d.setMinutes(d.getMinutes() + 10);
+        //         // d.getTime()
+        //         this.timeSlots[this.startTime] = (d.toLocaleTimeString())
+        //         console.log("Start Time",this.startTime )
+        //         // this.timeSlots.push(d)
+        //         console.log("time", d.toLocaleTimeString())
+        //         this.timeSlots.push(this.initialTime);
 
-<!-- <script>
-export default {
-    data() {
-        return {
-            intervals: 10,
-            i: 0,
-            times: [],
-            tt: 0,
-            ap: ['AM', 'PM'],
-            timeslots: {
-                startTime: '',
-                endTime: ''
-            }
-        }
+        //     }
+        //     console.log("newtime", this.timeSlots)
 
-    },
-    methods: {
-        addslots() {
-            for (i = 0; tt < 24 * 60; i++) {
-                var hh = Math.floor(tt / 60); // getting hours of day in 0-24 format
-                var mm = (tt % 60); // getting minutes of the hour in 0-55 format
-                times[i] = ("0" + (hh % 12)).slice(-2) + ':' + ("0" + mm).slice(-2) + ap[Math.floor(hh / 12)]; // pushing data in array in [00:00 - 12:00 AM/PM format]
-                tt = tt + x;
-                console.log(tt)
-            }
-        }
+        // }
     }
 }
+</script>
 
-</script> -->
